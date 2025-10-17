5dfb62a592b7a0670aa873b4bf4b518a458a73c1
Cesar Navarro <cnfrs9@gmail.com>
2025-10-17 20:31:37 -0300

    Implement findByDepartment and findAll methods in SellerDaoJDBC and include tests

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,37 @@
 package application;
 
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-	
+		SellerDao sd = DaoFactory.createSellerDao();
+		
+		System.out.println("\n==== TEST: seller findById ====");
+		Seller seller = sd.findById(3);
+		System.out.println(seller);
+		
+		System.out.println("\n==== TEST: seller findByDepartment ====");
+		Department dep = new Department(2, null);
+		List<Seller> list = sd.findByDepartment(dep);
+
+		for (Seller s : list) {
+			System.out.println(s);
+		}
+
+		System.out.println("\n==== TEST: seller findAll ====");
+		list = sd.findAll();
+
+		for (Seller s : list) {
+			System.out.println(s);
+		}
+
 	}
 
 }
