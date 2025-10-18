d33bb1e3bce3499bd52cd781e9b08d12ee08f9dd
Cesar Navarro <cnfrs9@gmail.com>
2025-10-18 18:27:30 -0300

    Implement deleteById method in SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sd = DaoFactory.createSellerDao();
 
@@ -44,6 +47,13 @@ public class Program {
 		seller.setEmail("clark@gmail.com");
 		sd.update(seller);
 		System.out.println(seller);
+
+		System.out.println("\n==== TEST: seller delete ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sd.deleteById(id);
+		System.out.println("Delete completed!");
+		sc.close();
 	}
 
 }
