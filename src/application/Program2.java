fd1b2c461e04eece11ecb6bdd421263e37df8df9
Cesar Navarro <cnfrs9@gmail.com>
2025-10-18 23:16:08 -0300

    Implement insert method in DepartmentDaoJDBC and add createDepartmentDao() to DaoFactory

---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		DepartmentDao dd = DaoFactory.createDepartmentDao();
+
+		System.out.println("\n==== TEST: department insert ====");
+		Department dep = new Department(null, "Food");
+		dd.insert(dep);
+		System.out.println(dep);
+
+	}
+
+}
